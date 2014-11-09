8879e7e6b94fb200c7f97f0e1b3ff7d9ac8c3df0
Gallyam <gallyamb@gmail.com>
2014-11-09 19:25:00 +0500

    Тесты и код для обработки параграфов

---
+++ Markdown.NET/Markdown.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Markdown.NET
+{
+    public class Markdown
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string Transform(string text)
+        {
+            text = StandartiseToUnixLineEndings(text);
+            text = StripEmptyLines(text);
+            text += "\n\n";
+            text = RemoveUnneccessaryNewLines(text);
+
+            text = ProcessParagraphs(text);
+            return text;
+        }
+
+        private static string ProcessParagraphs(string text)
+        {
+            var paragraphs = Regex.Split(text, "\n\n")
+                .Where(x => !String.IsNullOrWhiteSpace(x))
+                .Select(x => Regex.Replace(x, "\n", string.Empty));
+
+            text = "";
+
+            foreach (var paragraph in paragraphs)
+            {
+                text += String.Format("<p>{0}</p>", paragraph);
+            }
+            return text;
+        }
+
+        private static string RemoveUnneccessaryNewLines(string text)
+        {
+            return Regex.Replace(text, "\n{3,}", "\n\n");
+        }
+
+        private static string StripEmptyLines(string text)
+        {
+            return Regex.Replace(text, @"^[ \t]+$", string.Empty, RegexOptions.Multiline);
+        }
+
+        private static string StandartiseToUnixLineEndings(string text)
+        {
+            return text.Replace("\r\n", "\n").Replace("\r", "\n");
+        }
+    }
+}
---
+++ Markdown.NET/Tests/ModuleTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Markdown.NET.Tests
+{
+    [TestFixture]
+    public class ModuleTests
+    {
+        [Test]
+        public void ReturnStringWithoutSingleCarrageReturn()
+        {
+            var text = "Hello, \nworld!";
+            var expected = "<p>Hello, world!</p>";
+            Assert.AreEqual(expected, Markdown.Transform(text));
+        }
+        [Test]
+        public void ReturnParagraphForPlainText()
+        {
+            var text = "Hello, world!";
+            var expected = "<p>Hello, world!</p>";
+            Assert.AreEqual(expected, Markdown.Transform(text));
+        }
+
+        [Test]
+        public void Paragraph()
+        {
+            var text = "\n\nparagraph\n\n";
+            var expected = "<p>paragraph</p>";
+            Assert.AreEqual(expected, Markdown.Transform(text));
+        }
+
+        [Test]
+        public void ParagraphWithSpacesAndTabs()
+        {
+            var text = "\n\t\t  \n   \t\nparagraph\n\t\t\n  ";
+            var expected = "<p>paragraph</p>";
+            Assert.AreEqual(expected, Markdown.Transform(text));
+        }
+
+        [Test]
+        public void MultipleParagraphs()
+        {
+            var text = "\n\t  \n  \nfirst paragraph\n\nsecond paragraph\n\n";
+            var expected = "<p>first paragraph</p><p>second paragraph</p>";
+            Assert.AreEqual(expected, Markdown.Transform(text));
+        }
+
+        [Test]
+        public void ItalicTest()
+        {
+//            var text = 
+        }
+    }
+}
